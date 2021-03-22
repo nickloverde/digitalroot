48610b99403c55e2169bf21aa7d0ecf5994da07b
nickloverde <nick.loverde@gmail.com>
2021-03-22 11:21:03 -0500

    fixed error when returning 0 as a string

--- DigitalRoot.js
+++ DigitalRoot.js
@@ -9,12 +9,25 @@
 // Then, I will use the method reduce to act as an accumulator and add up the numbers (I will need to convert them back to numbers with parseInt)
 // Use an if statement to check if the sum of the numbers is a single digit. If it's not (sum > 9) then I need to rerun the function digital root with the sum as the parameter or else return the sum
 
+// function digital_root(number) {
+//     let arr = number.toString().split("");
+//     let sum = arr.reduce((a,b) => +(a) + +(b))
+ 
+//     if (sum > 9) {
+//         return digital_root(sum);
+//     } else {
+//         return sum;
+//     }
+//  }
+
 function digital_root(number) {
     let arr = number.toString().split("");
     let sum = arr.reduce((a,b) => +(a) + +(b))
  
     if (sum > 9) {
         return digital_root(sum);
+    } else if (sum === '0'){
+      return 0
     } else {
         return sum;
     }
